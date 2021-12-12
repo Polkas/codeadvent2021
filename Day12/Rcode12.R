03a40a1cac18486f408d418d05d1425ac5e0f4e2
Maciej Nasinski <nasinski.maciej@gmail.com>
2021-12-12 15:01:51 +0100

    day 12

---
+++ Day12/Rcode12.R
@@ -0,0 +1,46 @@
+d11 <- readLines("Day12/input12")
+
+library(igraph)
+library(collections)
+
+d11_df <- do.call(rbind, strsplit(d11, "-"))
+# gg <- graph_from_edgelist(d11_df, directed <- FALSE)
+# igraph::plot.igraph(gg)
+
+d11_1 <- tapply(d11_df[, 2], d11_df[, 1], list)
+d11_2 <- tapply(d11_df[, 1], d11_df[, 2], list)
+binded <- c(d11_1, d11_2)
+adjlist <- tapply(binded, names(binded), function(x) unique(unlist(x, FALSE, FALSE)))
+
+# is_lowercase <- function(x) x == tolower(x)
+is_uppercase <- function(x) x == toupper(x)
+
+rec_caves <- function(pos, from, to, caves, visited, allow_revisit_small) {
+  count <- 0
+  if (pos == to) {
+    return(1)
+  }
+  visited$push(pos)
+  for (cave in caves[[pos]]) {
+    if (cave == from) {
+      next
+    }
+
+    is_big <- is_uppercase(cave)
+    visited_before <- cave %in% visited$as_list()
+    revisited_small <- (!is_big) && visited_before
+
+    # new one, big one or small one second visit
+    allow_visit <- (!visited_before) || is_big || allow_revisit_small
+    # next one blocked
+    revisit_small <- allow_revisit_small && (!revisited_small)
+    if (allow_visit) count <- count + rec_caves(cave, from, to, caves, visited, revisit_small)
+  }
+  visited$pop()
+  return(count)
+}
+
+#1
+rec_caves("start", "start", "end", adjlist, collections::stack(), FALSE)
+#2
+rec_caves("start", "start", "end", adjlist, collections::stack(), TRUE)
